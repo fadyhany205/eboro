1af6808010fb908c0daa3ce876dc60d12b6fe10e
fady hany <fady.hany@santechture.com>
2025-01-26 16:59:19 +0200

    init

---
+++ src/main/java/com/eboro/EboroApplication.java
@@ -0,0 +1,13 @@
+package com.eboro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EboroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EboroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eboro/EboroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eboro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EboroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
